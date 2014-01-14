98c7576a33240c86a84a41ccf1f694ec3b5de466
Lars Huring <huring@Larss-MacBook-Pro.local>
2014-01-14 22:56:38 +0100

    Initial commit
    Testing character controller input and methods for basic platformer in
    Unity using new 2D tools.

---
+++ Assets/Scripts/enemyController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyController : MonoBehaviour {
+
+	public Transform sightStart, sightEnd;
+	public bool spotted = false;
+	public GameObject alert;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Raycasting ();
+		Behaviours ();
+
+	}
+
+	void Raycasting ()
+	{
+		Debug.DrawLine (sightStart.position, sightEnd.position, Color.green);
+		spotted = Physics2D.Linecast (sightStart.position, sightEnd.position);
+	}
+
+	void Behaviours ()
+	{
+		if (spotted) {
+						alert.SetActive (true);		
+				} else {
+			alert.SetActive(false);		
+		}
+	}
+}
